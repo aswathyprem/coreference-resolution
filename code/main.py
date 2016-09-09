19126b8df90bb38a0f115dccdc791c5c6e19a6b1
aswathyprem <aswathypremblogs@gmail.com>
2016-09-09 20:02:37 +0200

    Update main.py

--- code/main.py
+++ code/main.py
@@ -4,39 +4,42 @@ import markablesExtract
 
 # Method to extract training examples
 def extractTrain(coref_dict):
+    # initialize lists to store positive and negative pairs
     NP_positive = []
     NP_negative = []
+    
+    #iterate throught the list to access each document entries
     for i in range(len(coref_dict)):
+        
+        #save the document entries to a list
+        index = coref_dict[i+1]
         flag = 0
         counter = 0
+
+        #iterate through each NPs in the document
         for j in range(len(coref_dict[i+1])):
-            for k in range(len(coref_dict[i+1][j+1])):
-                NP_coref = coref_dict[i+1][j+1][k]
-                if NP_coref[1] != None and flag == 0:
-                    NP = NP_coref[0]
-                    temp = NP_coref[1]
-                    flag = 1
-                elif NP_coref[1] == None and flag !=0:
-                    counter = counter + 1
-                elif NP_coref[1] !=None and flag !=0:
-                    if set(NP_coref[1]) & set(temp):
-                        flag = 0
-                        NP_positive.append((NP,NP_coref[0]))
-                    else:
-                        NP_negative.append((NP,NP_coref[0]))
+            #repeat iteration for each element
+            for NP_coref in index[j:]: 
+                    if NP_coref[1] != None and flag == 0:
+                        NP = NP_coref[0]
+                        temp = NP_coref[1]
+                        flag = 1
+                    elif NP_coref[1] == None and flag !=0:
+                        counter = counter + 1
+                    elif NP_coref[1] !=None and flag !=0:
+                        if set(NP_coref[1]) & set(temp):
+                            flag = 0
+                            NP_positive.append((NP,NP_coref[0]))
+                        else:
+                            NP_negative.append((NP,NP_coref[0]))
+            
     print "NP - Positive"
     print NP_positive   
     
     print "NP - Negative"
     print NP_negative 
     
-                
-                        
-                        
-                    
-                    
-
-
+                                        
 
 if __name__ == "__main__":
     sys.stdout = open('file.log','w')
